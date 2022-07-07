ebc89f5202e0eff290f12159e47daf29332f80a6
nana <comfortappiah.ca@gmail.com>
2022-07-07 21:41:20 +0000

    virtual exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <WelcomeCodetrain name="Codetrain "/>
+      <WelcomeCodetrain name="KNUST "/>
+    
     </div>
   );
 }
 
 export default App;
+class  WelcomeCodetrain  extends Component {
+  render() {
+    return (
+			<>
+				<h1>{this.props.name} is awesome, react works!!!!</h1>
+			</>
+		);
+  }
+}
+
+
+
